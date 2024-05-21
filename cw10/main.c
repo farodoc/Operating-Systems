660ab8b4179a01268443662af8aa260e408a28e0
farodoc <panjakubkonopka@gmail.com>
2024-05-21 17:58:40 +0200

    Fixed reindeer's mutexes

--- cw10/main.c
+++ cw10/main.c
@@ -5,21 +5,10 @@
 
 int reindeer_count = 0;
 pthread_mutex_t santa_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t reindeer_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t reindeer_count_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t all_reindeer_back = PTHREAD_COND_INITIALIZER;
+pthread_cond_t all_reindeers_back = PTHREAD_COND_INITIALIZER;
 pthread_cond_t delivery_finished = PTHREAD_COND_INITIALIZER;
-
-pthread_mutex_t reindeers_mutexes[9] = {
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER,
-    PTHREAD_MUTEX_INITIALIZER
-};
 
 pthread_t threads[10];
 
@@ -29,21 +18,23 @@ void* reindeer(void* ID){
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
     while(1){
-        //sleep(rand() % 6 + 5);
-        sleep(rand() % 6 + 10);
+        sleep(rand() % 6 + 5);
         pthread_mutex_lock(&reindeer_count_mutex);
         reindeer_count++;
         printf("Renifer: czeka %d reniferów na Mikołaja, ID: %d\n", reindeer_count, id);
 
         if(reindeer_count == 9){
             printf("Renifer: wybudzam Mikołaja, ID: %d\n", id);
-            pthread_cond_signal(&all_reindeer_back);
+            pthread_cond_signal(&all_reindeers_back);
             reindeer_count = 0;
         }
 
         pthread_mutex_unlock(&reindeer_count_mutex);
 
-        pthread_cond_wait(&delivery_finished, &reindeers_mutexes[id]);
+        pthread_mutex_lock(&reindeer_mutex);
+        pthread_cond_wait(&delivery_finished, &reindeer_mutex);
+        pthread_mutex_unlock(&reindeer_mutex);
+
         printf("Renifer: lecę na wakacje, ID: %d\n", id);
     }
 
@@ -52,7 +43,8 @@ void* reindeer(void* ID){
 
 void* santa(){
     for(int i = 0; i < 4; i++){
-        pthread_cond_wait(&all_reindeer_back, &santa_mutex);
+        pthread_mutex_lock(&santa_mutex);
+        pthread_cond_wait(&all_reindeers_back, &santa_mutex);
         
         printf("Mikołaj: budzę się\n");
         printf("Mikołaj: dostarczam zabawki\n");
@@ -62,6 +54,7 @@ void* santa(){
         printf("Mikołaj: zasypiam\n");
 
         pthread_cond_broadcast(&delivery_finished);
+        pthread_mutex_unlock(&santa_mutex);
     }
 
     for(int i = 1; i < 10; i++){
